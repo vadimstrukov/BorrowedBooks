f37cd4bc8ae6b2dc37381b54ae89670c5adaeb70
vadimstrukov <vadim.strukov@gmail.com>
2016-11-21 02:06:36 +0200

    implemented OAuth2

---
+++ src/main/java/ee/strukov/books/api/config/OAuth2ServerConfiguration.java
@@ -0,0 +1,95 @@
+package ee.strukov.books.api.config;
+
+import ee.strukov.books.api.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+/**
+ * Created by strukov on 20.11.16.
+ */
+
+@Configuration
+public class OAuth2ServerConfiguration {
+
+    private static final String RESOURCE_ID = "restservice";
+
+    @Configuration
+    @EnableResourceServer
+    protected static class ResourceServerConfiguration extends
+            ResourceServerConfigurerAdapter {
+
+        @Override
+        public void configure(ResourceServerSecurityConfigurer resources) {
+            resources.resourceId(RESOURCE_ID);
+        }
+
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+                    .antMatchers("/api/v1/users").authenticated();
+        }
+
+    }
+
+    @Configuration
+    @EnableAuthorizationServer
+    protected static class AuthorizationServerConfiguration extends
+            AuthorizationServerConfigurerAdapter {
+
+        private TokenStore tokenStore = new InMemoryTokenStore();
+
+        @Autowired
+        @Qualifier("authenticationManagerBean")
+        private AuthenticationManager authenticationManager;
+
+        @Autowired
+        private UserServiceImpl userService;
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints)
+                throws Exception {
+            endpoints
+                    .tokenStore(this.tokenStore)
+                    .authenticationManager(this.authenticationManager)
+                    .userDetailsService(userService);
+        }
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients
+                    .inMemory()
+                    .withClient("clientapp")
+                    .authorizedGrantTypes("password", "refresh_token")
+                    .authorities("USER")
+                    .scopes("openid")
+                    .resourceIds(RESOURCE_ID)
+                    .secret("123456");
+        }
+
+        @Bean
+        @Primary
+        public DefaultTokenServices tokenServices() {
+            DefaultTokenServices tokenServices = new DefaultTokenServices();
+            tokenServices.setSupportRefreshToken(true);
+            tokenServices.setTokenStore(this.tokenStore);
+            return tokenServices;
+        }
+
+    }
+
+}
---
+++ src/main/java/ee/strukov/books/api/config/WebSecurityConfiguration.java
@@ -0,0 +1,33 @@
+package ee.strukov.books.api.config;
+
+import ee.strukov.books.api.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by strukov on 20.11.16.
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserServiceImpl userService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService);
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+}
