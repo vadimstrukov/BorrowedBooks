2463282a4c080c20e23f59e38134d1fe1c55ff69
vadimstrukov <vadim.strukov@gmail.com>
2016-11-22 03:03:09 +0200

    implemented logout

--- src/main/java/ee/strukov/books/api/controller/UserController.java
+++ src/main/java/ee/strukov/books/api/controller/UserController.java
@@ -6,8 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.InvalidClientException;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
@@ -20,13 +25,18 @@ public class UserController {
     @Autowired
     private UserServiceImpl userService;
 
+    @Autowired
+    private DefaultTokenServices defaultTokenServices;
+
     @RequestMapping(value = "/me", method = RequestMethod.GET)
     public ResponseEntity<User> getLoggedInUser(@AuthenticationPrincipal User user) {
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
-//    @RequestMapping(value = "/users", method = RequestMethod.POST)
-//    public ResponseEntity<User> registerUser(@RequestBody User user) {
-//        return new ResponseEntity<>(userService.save(user), HttpStatus.CREATED);
-//    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public @ResponseBody void logout(@RequestParam("token") String value) throws InvalidClientException {
+        defaultTokenServices.revokeToken(value);
+    }
+
 }
