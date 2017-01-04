7577bf9b4f75f688bdc218565dd18c26967e2c5b
vadimstrukov <vadim.strukov@gmail.com>
2017-01-04 20:10:41 +0200

    UserLibrary is Serializable now

--- src/main/java/ee/strukov/books/api/model/UserLibrary.java
+++ src/main/java/ee/strukov/books/api/model/UserLibrary.java
@@ -3,16 +3,18 @@ package ee.strukov.books.api.model;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * Created by strukov on 4.01.17.
  */
 @JsonSerialize
 @Data
-public class UserLibrary {
-    private Long ownedBooks;
-    private Long borrowedBooks;
-    public UserLibrary(Long ownedBooks, Long borrowedBooks){
-        this.borrowedBooks = borrowedBooks;
-        this.ownedBooks = ownedBooks;
+public class UserLibrary implements Serializable{
+    private Long ownedBooksLength;
+    private Long borrowedBooksLength;
+    public UserLibrary(Long ownedBooksLength, Long borrowedBooksLength){
+        this.borrowedBooksLength = borrowedBooksLength;
+        this.ownedBooksLength = ownedBooksLength;
     }
 }
