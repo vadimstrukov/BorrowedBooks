861c89968882275f6ceb92ed6f6429657a397bc4
vadimstrukov <vadim.strukov@gmail.com>
2017-08-07 21:18:06 +0300

    added @RestController to @BookApi annotation

--- src/main/java/ee/strukov/books/api/annotations/BookApi.java
+++ src/main/java/ee/strukov/books/api/annotations/BookApi.java
@@ -1,15 +1,19 @@
-package ee.strukov.books.api;
+package ee.strukov.books.api.annotations;
 
 import org.springframework.core.annotation.AliasFor;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.annotation.*;
+
+import static ee.strukov.books.api.BookApiConstants.BOOK_API_URL;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@RequestMapping("/api/v1/")
+@RequestMapping(BOOK_API_URL)
+@RestController
 public @interface BookApi {
     @AliasFor(annotation = RequestMapping.class, attribute = "request")
-    String request() default "";
+    String[] request() default {};
 }
