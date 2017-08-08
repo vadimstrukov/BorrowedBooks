3f656ad2897954568b23193246d089ddb235f095
vadimstrukov <vadim.strukov@gmail.com>
2017-08-08 21:12:53 +0300

    Refactored user controller

--- src/main/java/ee/strukov/books/api/controller/AuthUserController.java
+++ src/main/java/ee/strukov/books/api/controller/AuthUserController.java
@@ -3,33 +3,45 @@ package ee.strukov.books.api.controller;
 import ee.strukov.books.api.annotations.BookApi;
 import ee.strukov.books.api.annotations.BookApiRequest;
 import ee.strukov.books.api.model.User;
+import ee.strukov.books.api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.common.exceptions.InvalidClientException;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
+import static ee.strukov.books.api.BookApiConstants.BOOK_API_URL;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 /**
  * Created by strukov on 20.11.16.
  */
-@BookApi(request = "user")
+@BookApi(BOOK_API_URL + "user")
 public class AuthUserController {
 
 
     @Autowired
     private DefaultTokenServices defaultTokenServices;
 
-    @BookApiRequest(path = "me", method = GET)
+    @Autowired
+    private UserService userService;
+
+    @BookApiRequest(path = "/register", method = POST)
+    public User register(@RequestBody User user) {
+        return userService.register(user);
+    }
+
+    @BookApiRequest(path = "/me", method = GET)
     public User getLoggedInUser(@AuthenticationPrincipal User user) {
         return user;
     }
 
-    @BookApiRequest(path = "logout", method = GET)
+    @BookApiRequest(path = "/logout", method = GET)
     public String logout(HttpServletRequest request) throws InvalidClientException {
         defaultTokenServices.revokeToken(request.getHeader("Authorization").replace("Bearer", "").trim());
         return "Successfully logged out";
